0b74642008b50bc3392849528f1cd5b6e8ac49a9
Pawel Pindel <pawel.pindel@netsharpdev.com>
2023-05-08 22:16:34 +0200

    Initialize project

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { Container, Image, Nav, Navbar } from "react-bootstrap";
+const Header = () => {
+  const imageStyle = {
+    height: "50px",
+    marginRight: "10px",
+  };
+  return (
+    <Navbar id="mainNav" expand="lg" fixed="top" className="py-3 navbar-light">
+      <Container>
+        <Navbar.Brand href="#page-top">
+          <Image
+            style={imageStyle}
+            src="https://artmonkey.pl/img/1x/logo_1.png"
+            alt="Art Monkey logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="navbarResponsive">
+          <Nav as="ul" className="ml-auto my-2 my-lg-0">
+            <Nav.Item as="li">
+              <Nav.Link href="#about">O mnie</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link href="#services">Oferta</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link href="#contact">Kontakt</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+export default Header;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { Anchor, Col, Container, Row } from "react-bootstrap";
+
+const Hero = () => {
+  return (
+    <header className="masthead">
+      <Container className="h-100">
+        <Row className="h-100 align-items-center justify-content-center text-center">
+          <Col lg={10} className="align-self-end">
+            <h1 className="h1 text-uppercase text-white font-weight-bold text-center">
+              PROJEKTY WNĘTRZ
+            </h1>
+            <h1 className="h1 text-uppercase text-white font-weight-bold">
+              FOTOREALISTYCZNE WIZUALIZACJE
+            </h1>
+            <h1 className="h1 text-uppercase text-white font-weight-bold">
+              PROJEKTY MEBLI
+            </h1>
+            <hr className="divider my-4" />
+          </Col>
+          <Col lg={8} className="align-self-baseline">
+            <p className="text-white-75 font-weight-light mb-5">
+              <i>"Wolność pochodzi z wnętrza"</i> - Frank Lloyd Wright
+            </p>
+            <Anchor
+              className="btn btn-primary btn-xl js-scroll-trigger"
+              href="#about"
+            >
+              Dowiedz się więcej!
+            </Anchor>
+          </Col>
+        </Row>
+      </Container>
+    </header>
+  );
+};
+export default Hero;
